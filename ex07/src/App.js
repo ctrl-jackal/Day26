4928878443b5ab6534629178e83d7babcba2345b
Maid <maidsivac@gmail.com>
2021-04-02 10:19:46 +0200

    Adding tasks of day25

---
+++ ex07/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import PostRequestErrorHandling from "./GetRequestErrorHandling";
+
+function App() {
+  return (
+    <div className="App">
+      <PostRequestErrorHandling />
+    </div>
+  );
+}
+
+export default App;
